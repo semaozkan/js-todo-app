d28f24afb63c38728d5cbdd0c078dba9c3ad209c
Semanur Özkan <semanurozkan4444@gmail.com>
2023-08-14 01:14:50 +0300

    feat: create edit functionality

--- script.js
+++ script.js
@@ -50,6 +50,17 @@ window.addEventListener('load', () => {
 
         input.value = "";
 
+        task_edit_el.addEventListener("click", () => {
+            if(task_edit_el.innerText.toLowerCase() == "edit"){
+                task_input_el.removeAttribute("readonly")
+                task_input_el.focus()
+                task_edit_el.innerText = "Save"
+            }else{
+                task_input_el.setAttribute("readonly", "readonly")
+                task_edit_el.innerText = "Edit"
+            }
+        })
+
     })
 })
 
